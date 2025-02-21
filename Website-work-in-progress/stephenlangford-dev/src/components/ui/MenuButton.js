3193c3b26cb5066c02eb6e90acc88a464f8cad7f
Stephen-Langford <slangford1050@gmail.com>
2025-02-21 08:46:16 -0500

    finished ui work on start button

--- Website-work-in-progress/stephenlangford-dev/src/components/ui/MenuButton.js
+++ Website-work-in-progress/stephenlangford-dev/src/components/ui/MenuButton.js
@@ -5,17 +5,15 @@ const MenuButton = (props) => {
     const displayText = props.displayText;
 
     return (
-        <div className="float-left  mr-1.5 align-middle inline-block  px-1.5 bg-menu-gray border border-t-white border-l-white border-r-gray-500 border-b-gray-500  w-59px text-black font-normal shadow-black shadow">
-            <span className="w-11 h-3.5 font-bold">
-                <Image
-                    src="/icons/windows-4.png"
-                    alt="Windows Icon"
-                    width={15} // Adjust size as needed
-                    height={15} // Ensures proper aspect ratio
-                    className=" object-contain inline-block pr-0.5"
-                />
-                {displayText}
-            </span>
+        <div className="float-left m-0 align-middle  px-1.5 py-0 bg-menu-gray border border-t-white border-l-white border-r-gray-500 border-b-gray-500 w-59px text-black font-normal shadow-black shadow flex flex-row items-center">
+            <Image
+                src="/icons/windows-4.png"
+                alt="Windows Icon"
+                width={16} // Adjust size as needed
+                height={16} // Ensures proper aspect ratio
+                className=""
+            />
+            <span className="font-bold pl-0.5">{displayText}</span>
         </div>
     );
 };
